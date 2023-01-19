0085aa77fc938a346a01bef01f6993e8cac26784
Yeferson hernandez <yeyehernandez.0809gmail.com>
2023-01-19 16:04:24 -0500

    rutas anidadas listas

---
+++ src/01-laziload/pages/NoFount.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NoFount = () => {
+  return (
+    <h1>No found</h1>
+  )
+}
+
+export default NoFount
--- src/routers/routes.ts
+++ src/routers/routes.ts
@@ -1,6 +1,7 @@
 import { lazy, LazyExoticComponent } from "react";
-// import { LazyPage1, LazyPage2, LazyPage3 } from "../01-laziload/pages";
-import {  LazyPage3 } from "../01-laziload/pages";
+
+import NoFount from "../01-laziload/pages/NoFount";
+
 
 type JSXComponent = ()=>JSX.Element
 
@@ -11,29 +12,23 @@ interface Route {
     name: string
 }
 
-const Lazy1 = lazy(()=> import(/* webpackChunkName: "LazyPage1"*/'../01-laziload/pages/LazyPage1'));
-const Lazy2 = lazy(()=> import(/* webpackChunkName: "LazyPage2"*/'../01-laziload/pages/LazyPage2'));
-const Lazy3 = lazy(()=> import(/* webpackChunkName: "LazyPage3"*/'../01-laziload/pages/LazyPage3'));
+const LazyLayout = lazy(()=> import(/* webpackChunkName: "LazyLayout"*/'../01-laziload/lazyLayout/LazyLayout'));
+
 
 export const routes: Route[] = [
     {
-        to: '/lazi1',
-        path: 'lazi1',
-        Component: Lazy1,
-        name: 'lazi-1',
+        path: '/laziload/*',
+        to: '/laziload/',
+        Component: LazyLayout,
+        name: 'lazi-Layout',
     },
     {
-        to: '/lazi2',
-        path: 'lazi2',
-        Component: Lazy2,
-        name: 'lazi-2',
+        to: '/no-found',
+        path: 'no-found',
+        Component: NoFount,
+        name: 'no-found',
     },
-    {
-        to: '/lazi3',
-        path: 'lazi3',
-        Component: Lazy3,
-        name: 'lazi-3',
-    }
+  
 ];
 
 
